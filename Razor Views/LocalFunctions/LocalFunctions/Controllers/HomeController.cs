244cfb324fcce53bf6861377a735972b95651d17
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-04-20 17:08:12 +0200

    Razor

---
+++ Razor Views/LocalFunctions/LocalFunctions/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LocalFunctions.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Rawhtml()
+        {
+            return View("HtmlRaw");
+        }
+    }
+}
