60ced03005f861ed7dc724d139b7f53e0ad8d988
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-08-11 12:10:21 +0300

    Dependency Injection

---
+++ Section 10- Dependency Injection/Dependency Injection/Service/CityService.cs
@@ -0,0 +1,27 @@
+﻿using ServiceContract;
+
+namespace Services
+{
+	public class CityService:ICityService
+	{
+		private List<string> Cities;
+
+		public CityService()
+		{
+			Cities = new List<string>()
+			{
+				"aa",
+				"bb",
+				"cc",
+				"dd",
+				"ee",
+				"ff"
+			};
+		}
+
+		public List<string> GetCountries()
+		{
+			return Cities;
+		}
+	}
+}
