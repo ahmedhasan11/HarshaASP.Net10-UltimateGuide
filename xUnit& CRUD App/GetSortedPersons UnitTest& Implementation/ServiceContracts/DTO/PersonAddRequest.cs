3b17b1839a3360bd762d7549bdbaa73be2d993bf
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-08-26 15:48:56 +0300

    GetSortedPersons

---
+++ xUnit& CRUD App/GetSortedPersons UnitTest& Implementation/ServiceContracts/DTO/PersonAddRequest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using ServiceContracts.Enums;
+namespace ServiceContracts.DTO
+{
+	/// <summary>
+	/// here you put the propertiesthat you want the user to add , that's why we separatethe model by making DTO's
+	/// so Model:contain all properties thatyou want to store in DB table
+	/// DTO:properties that enetered by the user
+	/// 
+	/// 
+	/// DTO forinserting a newperson
+	/// </summary>
+    public class PersonAddRequest
+    {
+		[Required(ErrorMessage ="Name is Required")]
+		public string? PersonName { get; set; }
+
+		[EmailAddress(ErrorMessage ="email should be in correct format")]
+		[Required(ErrorMessage ="email is required")]
+		public string? EmailAddress { get; set; }
+		public DateTime? DateOfBirth { get; set; }
+		public GenderOptions? Gender { get; set; } //makeenum here only because here is the request properties
+		public string? Address { get; set; }
+
+		public Guid? CountryID { get; set; } //FK
+		public bool? ReccivenewsLetters { get; set; }
+		public Person ToPerson()
+		{
+			Person person = new Person()
+			{
+				PersonName = PersonName,
+				Address = Address,
+				CountryID = CountryID,
+				DateOfBirth = DateOfBirth,
+				Gender = Gender.ToString(),
+				EmailAddress = EmailAddress,
+				ReccivenewsLetters=ReccivenewsLetters
+			};
+
+			return person;
+		}
+	}
+}
