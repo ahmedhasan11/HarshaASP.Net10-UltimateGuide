9d9ba83d760b0d9795f5ff94b28797ebc489ae15
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-09-28 15:10:15 +0300

    Fluent API

---
+++ EntityFrameworkCore/FluentAPI P1&P2/Entities/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Entities
+{
+   public class Person
+    {
+		[Key]
+        public Guid PersonID { get; set; }
+
+		[StringLength(40)]
+        public string? PersonName { get; set; }
+		[StringLength(40)]
+		public string? EmailAddress { get; set; }
+		public DateTime? DateOfBirth { get; set; }
+		[StringLength(10)]
+		public string? Gender { get; set; }
+		[StringLength(300)]
+		public string? Address { get; set; }
+
+		public Guid? CountryID { get; set; } //FK
+		public bool? ReccivenewsLetters { get; set; }
+
+		public string? TIN { get; set; }
+
+	}
+}
---
+++ EntityFrameworkCore/FluentAPI P1&P2/Entities/PersonsDbContext.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+   public class PersonsDbContext:DbContext
+    {
+        public PersonsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+		public DbSet<Person> Persons { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			//FLUENT API :Table Name
+
+			modelBuilder.Entity<Country>().ToTable("Countries");
+
+			modelBuilder.Entity<Person>().ToTable("Persons");
+
+			//FLUENT API :Seeding data
+			string CountriesJson = System.IO.File.ReadAllText("countries.json");
+
+			List<Country> Countries=System.Text.Json.JsonSerializer.Deserialize<List<Country>>(CountriesJson);
+
+			foreach (Country country in Countries)
+				modelBuilder.Entity<Country>().HasData(country);
+
+
+			string PersonsJson=System.IO.File.ReadAllText("persons.json");
+			List<Person> Persons = System.Text.Json.JsonSerializer.Deserialize<List<Person>>(PersonsJson);
+
+			foreach(Person person in Persons)
+			modelBuilder.Entity<Person>().HasData(person);
+
+
+			modelBuilder.Entity<Person>().Property(p => p.TIN).HasColumnName("TaxIdentificationNumber").HasColumnType("varchar(40)").HasDefaultValue("ABC12345");
+			//FLUENT API : if you need the TIN column all its values to be unique
+			//modelBuilder.Entity<Person>().HasIndex().IsUnique();
+			//FLUENT API : if you need the TIN 
+			modelBuilder.Entity<Person>().HasCheckConstraint("CHK_TIN", "len([TaxIdentificationNumber])=8");
+
+
+
+
+		}
+
+		public List<Person> sp_GetAllPersons()
+		{
+			return Persons.FromSqlRaw("EXEC [dbo].[GetAllPersons]").ToList();
+		}
+
+		public int sp_InsertPerson(Person person)
+		{
+			SqlParameter[] sqlParameters = new SqlParameter[]
+			{
+				new SqlParameter("@PersonID",person.PersonID),
+				new SqlParameter("@PersonName",person.PersonName),
+				new SqlParameter("@EmailAddress",person.EmailAddress),
+				new SqlParameter("@DateOfBirth",person.DateOfBirth),
+				new SqlParameter("@Gender",person.Gender),
+				new SqlParameter("@Address",person.Address),
+				new SqlParameter("@CountryID",person.CountryID),
+				new SqlParameter("@ReccivenewsLetters",person.ReccivenewsLetters)
+			};
+
+			return Database.ExecuteSqlRaw("EXEC [dbo].[InsertPerson] @PersonID ,@PersonName,@EmailAddress,@DateOfBirth,@Gender,@Address,@CountryID,@ReccivenewsLetters"
+				,sqlParameters);
+
+
+		}
+	}
+}
