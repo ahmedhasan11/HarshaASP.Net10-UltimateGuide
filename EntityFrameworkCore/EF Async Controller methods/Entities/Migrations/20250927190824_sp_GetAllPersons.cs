4201fec5052bbb5ba65c7334819c02823c60bb52
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-09-29 17:45:34 +0300

    async controller methods

---
+++ EntityFrameworkCore/EF Async Controller methods/Entities/Migrations/20250927190824_sp_GetAllPersons.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class sp_GetAllPersons : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string sp_GetAllPersons = @"CREATE PROCEDURE [dbo].[GetAllPersons]
+                                        AS BEGIN
+                                        SELECT PersonID, PersonName, EmailAddress, 
+                                        DateOfBirth, Gender,Address,CountryID,ReccivenewsLetters
+                                        FROM [dbo].[Persons]
+                                        END";
+
+            migrationBuilder.Sql(sp_GetAllPersons);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+			string sp_GetAllPersons = @"DROP PROCEDURE [dbo].[GetAllPersons]
+                                        AS BEGIN
+                                        SELECT PersonID, PersonName, EmailAddress, 
+                                        DateOfBirth, Gender,Address,CountryID,ReccivenewsLetters
+                                        FROM [dbo].[Persons]
+                                        END";
+
+			migrationBuilder.Sql(sp_GetAllPersons);
+		}
+    }
+}
