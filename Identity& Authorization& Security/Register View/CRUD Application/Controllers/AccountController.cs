b2415891d4fb3f732ea1f492c756b16ae0e8896e
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-10-01 18:27:16 +0300

    Register view

---
+++ Identity& Authorization& Security/Register View/CRUD Application/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceContracts.DTO;
+
+namespace CRUD_Application.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(RegisterDTO registerDTO)
+        {
+            //storeuser registeration details into identity DB 
+            return RedirectToAction(nameof(PersonsController.Index), "Persons");
+        }
+    }
+}
