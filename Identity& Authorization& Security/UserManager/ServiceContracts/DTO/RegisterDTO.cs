bc86e764f6794d9a27da0186bb2b6d2bc3ab59f0
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-10-01 22:26:19 +0300

    user manager

---
+++ Identity& Authorization& Security/UserManager/ServiceContracts/DTO/RegisterDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts.DTO
+{
+    public class RegisterDTO
+    {
+		[Required(ErrorMessage ="Person Name can't be blank")]		
+		public string PersonName { get; set; }
+		[Required(ErrorMessage = "Email can't be blank")]
+		[EmailAddress(ErrorMessage ="Email should be in proper email address Format") ]
+		public string Email { get; set; }
+		[Required(ErrorMessage = "Phone Number can't be blank")]
+		[DataType(DataType.PhoneNumber)]
+		public string Phone { get; set; }
+		[Required(ErrorMessage = "Password can't be blank")]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+		[Required(ErrorMessage = "Confirm Password Name can't be blank")]
+		[DataType(DataType.Password)]
+		public string ConfirmPassword { get; set; }
+
+	}
+}
