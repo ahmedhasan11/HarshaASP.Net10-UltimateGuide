93ae71caa145c4ce7588f5909017b5a5cdf62ad8
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-10-01 16:27:02 +0300

    ApplicationUser& Role& IdentityDbContext

---
+++ Identity& Authorization& Security/Create Identity Models/Entities/Migrations/20250925191027_seed country.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entities.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedcountry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: new Guid("12e15727-d369-49a9-8b13-bc22e9362179"),
+                column: "Countryname",
+                value: "China");
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: new Guid("14629847-905a-4a0e-9abe-80b61655c5cb"),
+                column: "Countryname",
+                value: "Philippines");
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: new Guid("501c6d33-1bbe-45f1-8fbd-2275913c6218"),
+                column: "Countryname",
+                value: "China");
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: new Guid("56bf46a4-02b8-4693-a0f5-0a95e2218bdc"),
+                column: "Countryname",
+                value: "Thailand");
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: new Guid("8f30bedc-47dd-4286-8950-73d8a68e5d41"),
+                column: "Countryname",
+                value: "Palestinian Territory");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: new Guid("12e15727-d369-49a9-8b13-bc22e9362179"),
+                column: "Countryname",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: new Guid("14629847-905a-4a0e-9abe-80b61655c5cb"),
+                column: "Countryname",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: new Guid("501c6d33-1bbe-45f1-8fbd-2275913c6218"),
+                column: "Countryname",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: new Guid("56bf46a4-02b8-4693-a0f5-0a95e2218bdc"),
+                column: "Countryname",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "CountryID",
+                keyValue: new Guid("8f30bedc-47dd-4286-8950-73d8a68e5d41"),
+                column: "Countryname",
+                value: null);
+        }
+    }
+}
---
+++ Identity& Authorization& Security/Create Identity Models/ServiceContracts/IPersonService.cs
@@ -0,0 +1,53 @@
+﻿using Entities;
+using ServiceContracts.DTO;
+using ServiceContracts.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts
+{
+    public interface IPersonService
+    {
+        /// <summary>
+        /// adds newpersonto the personslist
+        /// </summary>
+        /// <param name="personAddRequest"></param>
+        /// <returns>returns person details with newlygenerated ID</returns>
+
+        Task<PersonResponse> AddPerson(PersonAddRequest? personAddRequest);
+
+        /// <summary>
+        /// returns all persons 
+        /// </summary>
+        /// <returns></returns>
+        /// 
+
+        // List<PersonResponse> GetAllPersons();
+
+        Task<PersonResponse> GetPersonByID(Guid? personID);
+
+        Task<List<PersonResponse>> GetAllPersons();
+
+        //SearchBy --> the property
+        //SearchString-->the value
+        Task<List<PersonResponse>> GetFilteredPersons(string SearchBy, string? SearchString);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="allPersons">repersents list of persons which will be sorted</param>
+        /// <param name="SortBy"></param>
+        /// <param name="SortOrder"></param>
+        /// <returns></returns>
+        Task<List<PersonResponse>> GetSortedPersons(List<PersonResponse> allPersons,string SortBy, SortOrderEnum SortOrder);
+        Task<PersonResponse> UpdatePerson(PersonUpdateRequest? personUpdateRequest);
+
+        Task<bool> DeletePerson(Guid? PersonID); //bool here tell you if deletion is successful or not
+
+
+
+    }
+}
