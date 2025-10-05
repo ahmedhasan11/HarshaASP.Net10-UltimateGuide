e42f2d0ea1d2f6cf8630773a331beddb457b33a4
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-10-05 17:57:13 +0300

    Admin Area

---
+++ Identity& Authorization& Security/Areas/CRUD Application/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_Application.Areas.Admin.Controllers
+{
+    [Area("Admin")] //to specify that this area is related to Admin
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
