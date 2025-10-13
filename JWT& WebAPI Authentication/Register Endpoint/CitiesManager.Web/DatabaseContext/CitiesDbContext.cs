87c3fdd8a7d93af82a479c49a54b67ddcc910c37
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-10-13 13:53:05 +0300

    Register Endpoint

---
+++ JWT& WebAPI Authentication/Register Endpoint/CitiesManager.Web/DatabaseContext/CitiesDbContext.cs
@@ -0,0 +1,28 @@
+﻿using CitiesManager.Web.Identity;
+using CitiesManager.Web.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace CitiesManager.Web.DatabaseContext
+{
+	public class CitiesDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+	{
+		public CitiesDbContext(DbContextOptions options):base(options)
+		{
+		}
+
+		public CitiesDbContext()
+		{
+		}
+		public DbSet<City> Cities{get;set;}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<City>().HasData(new City() { CityID= Guid.Parse("{21D68AA7-7214-4194-9662-95703EA61D4B}"), CityName="New York"});
+			modelBuilder.Entity<City>().HasData(new City() { CityID = Guid.Parse("{92CB40DB-A930-482F-84C0-4D1538A01CC4}"), CityName = "London" });
+		}
+	}
+}
---
+++ JWT& WebAPI Authentication/Register Endpoint/CitiesManager.Web/Identity/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CitiesManager.Web.Identity
+{
+	public class ApplicationUser:IdentityUser<Guid>
+	{
+		//(ID,Email,UserName,PhoneNumber,PasswordHash)
+	
+		public string? PersonName { get; set; }
+	}
+}
