5754c2b3552c65bf88a5857e3e84bdaf6716f528
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-10-14 18:42:23 +0300

    JWTServiceCOntracts& JWtService

---
+++ JWT& WebAPI Authentication/Generate JWT token (JwtService)/CitiesManager.Web/DTO/AuthenticationResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace CitiesManager.Web.DTO
+{
+	public class AuthenticationResponse
+	{
+		//here wewillput the propertes that youwill return as response after the token 
+		//have been generated
+		public string? PersonName { get; set; }
+		public string? Email { get; set; }
+		public string? Token { get; set; }
+		public DateTime Expiration { get; set; }
+	}
+}
---
+++ JWT& WebAPI Authentication/Generate JWT token (JwtService)/CitiesManager.Web/ServiceContracts/IJwtService.cs
@@ -0,0 +1,10 @@
+﻿using CitiesManager.Web.DTO;
+using CitiesManager.Web.Identity;
+
+namespace CitiesManager.Web.ServiceContracts
+{
+	public interface IJwtService
+	{
+		AuthenticationResponse CreateJwtToken(ApplicationUser user);
+	}
+}
