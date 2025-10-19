bcd963d58e1bfcbbf9330300a709be261d22d1da
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-10-19 12:22:56 +0300

    Repository Implementation for Person& Country

---
+++ Repository Pattern/Repository Implementation/Repositories/PersonsRepository.cs
@@ -0,0 +1,69 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using RepositoryContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+	public class PersonsRepository : IPersonsRepository
+	{
+		private readonly PersonsDbContext _context;
+		public PersonsRepository(PersonsDbContext context)
+		{
+			_context = context;
+		}
+		public async Task<Person> AddPerson(Person person)
+		{
+			_context.Persons.Add(person);
+			await _context.SaveChangesAsync();
+			return person;
+		}
+
+		public async Task<bool> DeletePersonByPersonID(Guid personID)
+		{
+			_context.Persons.RemoveRange(_context.Persons.Where(p => p.PersonID == personID));
+			int rowsDeleted= await _context.SaveChangesAsync();
+			return rowsDeleted>0;
+		}
+		public async Task<List<Person>> GetAllPersons()
+		{
+			return await _context.Persons.Include(p=>p.Country).ToListAsync();
+		}
+		public async Task<Person?> GetPersonByPersonID(Guid personID)
+		{
+			return await _context.Persons.Include(p=>p.Country).FirstOrDefaultAsync(p => p.PersonID == personID);
+		}
+
+
+		public async Task<List<Person>> GetFilteredPersons(Expression<Func<Person, bool>> predicate)
+		{
+			return await _context.Persons.Include(p => p.Country)
+				.Where(predicate).ToListAsync();
+			/*where(predicate) -->whatever lambda expression in the
+			parameter will get applied successfully to the query*/
+		}
+
+		public async Task<Person> UpdatePersonByPersonID(Person person)
+		{
+			Person? ActualPerson= await _context.Persons.FirstOrDefaultAsync(p => p.PersonID == person.PersonID);
+			if (ActualPerson==null)
+			{
+				return person;
+			}
+			ActualPerson.PersonName = person.PersonName;
+			ActualPerson.EmailAddress = person.EmailAddress;
+			ActualPerson.DateOfBirth = person.DateOfBirth;
+			ActualPerson.CountryID = person.CountryID;
+			ActualPerson.ReccivenewsLetters = person.ReccivenewsLetters;
+			ActualPerson.Gender = person.Gender;
+			ActualPerson.Address = person.Address;
+			int countUpdated= await _context.SaveChangesAsync();
+			return ActualPerson;
+		}
+	}
+}
---
+++ Repository Pattern/Repository Implementation/Services/CountryService.cs
@@ -0,0 +1,90 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using ServiceContracts;
+using ServiceContracts.DTO;
+
+namespace Services
+{
+	public class CountryService : ICountryService
+	{
+		private PersonsDbContext _db;
+
+		public CountryService(PersonsDbContext db)
+		{
+			_db = db;
+
+		}
+
+		public async Task<CountryResponse> AddCountry(CountryAddRequest? countryAddRequestobj)
+		{
+			//create validations for the unit test to check that everythingis alright
+			//1-validation ofr obj is null
+			if (countryAddRequestobj == null)
+			{
+				throw new ArgumentNullException(nameof(countryAddRequestobj));
+			}
+			//2-validatoion of country name is null
+			if (countryAddRequestobj.Countryname == null)
+			{
+				throw new ArgumentException(nameof(countryAddRequestobj.Countryname));
+			}
+			//3-validation for duplicate country namne
+			if (await _db.Countries.CountAsync(temp => temp.Countryname == countryAddRequestobj.Countryname) > 0)
+			{
+				throw new ArgumentException("nameof country isalready exists");
+			}
+			//convert CountryAddRequest obj into Country obj
+			Country country=countryAddRequestobj.ToCountry();
+
+			//give the obj an id
+			country.CountryID = Guid.NewGuid();
+
+			//addingthe country to the list
+
+			_db.Countries.Add(country);
+			await _db.SaveChangesAsync();
+			//converting country into countryresponse to return it
+			CountryResponse countryResponse = country.ToCountryResponse();
+
+			return countryResponse;
+
+		}
+
+		public async Task<List<CountryResponse>> GetAllCountries() //retrieve List of countryresponse that contains  all the countries
+		{
+			//List<CountryResponse> countryResponses = new List<CountryResponse>();
+
+			//foreach (Country country in  _db.Countries)
+			//{
+			//	countryResponses.Add(country.ToCountryResponse());
+			//}
+
+
+			return await _db.Countries.Select(country => country.ToCountryResponse()).ToListAsync();
+
+			//return countryResponses;
+
+			//return _countries.Select(country => country.ToCountryResponse()).ToList(); -->same code as above
+			//_countries.Select(country => country.ToCountryResponse()).ToList()-->returns list of CountryResponse
+
+		}
+
+		public async Task< CountryResponse?> GetCountryByID(Guid? countryID)
+		{
+			if (countryID==null)
+			{
+				return null;
+			}
+
+			//getting the country
+			Country? country = await _db.Countries.Where(country => country.CountryID == countryID).FirstOrDefaultAsync();
+			if (country == null)
+			{
+				return null;
+
+			}
+			CountryResponse response = country.ToCountryResponse();
+			return response;
+		}
+	}
+}
