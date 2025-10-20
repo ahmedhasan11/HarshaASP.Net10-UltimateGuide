253983f084bff9c05babc7de5adf44738d3c227b
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-10-20 16:17:06 +0300

    Seq Sink , making Live logs on Seq server

---
+++ Logging and Serilog/Serilog Seq Sink/RepositoryContracts/ICountriesRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+
+namespace RepositoryContracts
+{
+	/// <summary>
+	/// Represents data access logic for managing country entity
+	/// </summary>
+	public interface ICountriesRepository
+	{
+		Task<Country> AddCountry(Country country);
+		Task<List<Country>> GetAllCountries();
+
+		Task<Country?> GetCountryByCountryID(Guid countryID);
+
+		Task<Country?> GetCountryByCountryName(string countryName);
+
+	}
+}
