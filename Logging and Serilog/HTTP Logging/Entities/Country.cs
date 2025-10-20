d783bc45653217810d938d3804de19bdb641f49e
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-10-20 12:53:48 +0300

    enable HTTP Logging& configure default Log to show request and response properties

---
+++ Logging and Serilog/HTTP Logging/Entities/Country.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+	/// <summary>
+	/// Domain model for Country (DB Model)
+	/// </summary>
+	public class Country
+	{
+		//put here the properties that will be stored for the country modelin the DB
+		[Key]
+		public Guid CountryID { get; set; }
+
+		public string? Countryname { get; set; }
+
+		public virtual ICollection<Person>? Persons { get; set; }
+
+	}
+}
