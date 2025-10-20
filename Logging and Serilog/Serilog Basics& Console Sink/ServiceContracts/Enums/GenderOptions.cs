a9001ec6eba70ee00f995bb064797c68e8afd0b7
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-10-20 14:19:56 +0300

    Serilog service in IOC container , Configuring the Serilog, Console Sink

---
+++ Logging and Serilog/Serilog Basics& Console Sink/ServiceContracts/Enums/GenderOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts.Enums
+{
+    //putit onlyin the request person DTO
+    public enum GenderOptions
+    {
+        male,female , Other
+    }
+}
