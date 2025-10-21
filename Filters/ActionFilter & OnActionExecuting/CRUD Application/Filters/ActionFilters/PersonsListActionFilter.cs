15344089dc14f3dbe1043b8586a396c4b8d3ec9b
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-10-21 18:38:42 +0300

    Understanding Filter flow , ActionFilter methods , made parameter validation on searchBy parameter of Index in OnActionExecuting

---
+++ Filters/ActionFilter & OnActionExecuting/CRUD Application/Filters/ActionFilters/PersonsListActionFilter.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using ServiceContracts.DTO;
+
+namespace CRUD_Application.Filters.ActionFilters
+{
+	public class PersonsListActionFilter : IActionFilter
+	{
+		private readonly ILogger<PersonsListActionFilter> _logger;
+
+		public PersonsListActionFilter(ILogger<PersonsListActionFilter> logger)
+		{
+			_logger = logger;
+		}
+		public void OnActionExecuted(ActionExecutedContext context)
+		{
+			//AFTER logic
+			_logger.LogInformation("PersonsListActionFilter.OnActionExecuted()");
+		}
+
+		public void OnActionExecuting(ActionExecutingContext context)
+		{
+			//BEFORE logic
+			_logger.LogInformation("PersonsListActionFilter.OnActionExecuting()");
+
+			if (context.ActionArguments.ContainsKey("searchBy"))
+			{
+				string? searchBy = Convert.ToString(context.ActionArguments["searchBy"]);
+				if (!string.IsNullOrEmpty(searchBy))
+				{
+					var searchoptions = new List<string>()
+					{
+					nameof(PersonResponse.PersonName),
+					nameof(PersonResponse.Gender),
+					nameof(PersonResponse.Address),
+					nameof(PersonResponse.CountryID),
+					nameof(PersonResponse.DateOfBirth),
+					nameof(PersonResponse.EmailAddress),
+					};
+					if (searchoptions.Any(temp => temp == searchBy) == false)
+					{
+						_logger.LogInformation("value of {searchBy}", searchBy);
+						context.ActionArguments["searchBy"] = nameof(PersonResponse.PersonName);
+						_logger.LogInformation("value of {searchBy}", context.ActionArguments["searchBy"]);
+					}
+				}
+			}			
+		}
+	}
+}
