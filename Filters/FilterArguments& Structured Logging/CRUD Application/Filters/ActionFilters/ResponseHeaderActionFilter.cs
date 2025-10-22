b434de3cd811f5e9cb7024e7b8c88d6ef5077454
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-10-22 13:50:29 +0300

    Strcutured logging instead of normal logging in both action filters classes we did , understood the Arguments of TypeFilter

---
+++ Filters/FilterArguments& Structured Logging/CRUD Application/Filters/ActionFilters/ResponseHeaderActionFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CRUD_Application.Filters.ActionFilters
+{
+	public class ResponseHeaderActionFilter:IActionFilter
+	{
+		private readonly ILogger<ResponseHeaderActionFilter> _logger;
+		private readonly string Key;
+		private readonly string Value;
+
+		public ResponseHeaderActionFilter(ILogger<ResponseHeaderActionFilter> logger, string key, string value)
+		{
+			_logger = logger;
+			Key= key;
+			Value= value;
+		}
+
+		public void OnActionExecuted(ActionExecutedContext context)
+		{
+			//structured logging
+			_logger.LogInformation("{FilterName}.{MethodName}",nameof(ResponseHeaderActionFilter),nameof(OnActionExecuted));
+		}
+
+		public void OnActionExecuting(ActionExecutingContext context)
+		{
+			//structured logging
+			_logger.LogInformation("{FilterName}.{MethodName}", nameof(ResponseHeaderActionFilter), nameof(OnActionExecuting));
+			context.HttpContext.Response.Headers[Key]=Value;
+		}
+	}
+}
