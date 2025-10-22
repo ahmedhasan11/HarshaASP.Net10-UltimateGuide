d91ae1fd9b09410cb3a54e43594d8c6cca7f1e29
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-10-22 19:25:06 +0300

    making ResponseHeader Filter as Global filter in program.cs & diff between IActionFilter and IAsyncActionFilter

---
+++ Filters/Global Filters& AsyncFilters/CRUD Application/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_Application.Areas.Admin.Controllers
+{
+    [Area("Admin")] //to specify that this area is related to Admin
+    [Authorize(Roles ="Admin")] //only adminis allowedto accessthese action methods of this controller 
+    //can be ona specific actionmethod
+    //can also be on the whole controller
+    //so if a normal user try to request this actionmethod using query string he get accessdenied
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
