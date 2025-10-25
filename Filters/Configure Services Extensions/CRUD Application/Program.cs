0619ba52c89acd18825d1b6f2dd2d6d3aab3d2f8
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-10-25 18:16:27 +0300

    cleaning program.cs from Ioc container services & adding them as a separate file & call them using Extension method

---
+++ Filters/Configure Services Extensions/CRUD Application/Program.cs
@@ -0,0 +1,65 @@
+using Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rotativa.AspNetCore;
+using Services;
+using ServiceContracts;
+using RepositoryContracts;
+using Repositories;
+using ServiceContracts.DTO;
+using System.Runtime;
+using Serilog;
+using Serilog.AspNetCore;
+using CRUD_Application.Filters.ActionFilters;
+using CRUD_Application.StartupExtensions;
+namespace CRUD_Application
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Host.UseSerilog((HostBuilderContext context, IServiceProvider service, LoggerConfiguration logger_configuration) =>
+            {
+                logger_configuration.ReadFrom.Configuration(context.Configuration).ReadFrom.Services(service);
+            });
+            //Extension method for all IOC container Services for clean code
+            builder.Services.ConfigureServices(builder.Configuration);
+   
+            var app = builder.Build();
+            app.UseSerilogRequestLogging();//to add the completion log //look at IDiagnosticContext
+            app.UseHttpLogging();
+            Rotativa.AspNetCore.RotativaConfiguration.Setup("wwwroot",wkhtmltopdfRelativePath:"Rotativa");
+            app.UseHsts();
+            app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			app.UseRouting();
+			app.UseAuthentication();//tofetch thecookie and wecan then use the @User to display username after login
+            app.UseAuthorization();//-->addsauthorization middleware to the request pipeline which addsthe access permissionsof theparticularuser-->it evaluateswhich the currentuser can accessthisparticularresourse or not
+            app.MapControllers();
+            app.UseEndpoints(endpoints =>
+            {
+				//conventional routing for Areas
+				endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}"); //Admin/Home/Index,,,,,,,,,area here means area name
+
+
+                //conventional routing for all controllers Folder
+                endpoints.MapControllerRoute(
+                    name: "default",
+					pattern: "{controller}/{action}/{id?}");
+
+
+
+			});
+
+            app.Run();
+        }
+    }
+}
