3ab9596d6a020adea3b4b9ab1aa5313252c70f61
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-10-26 21:40:16 +0300

    SOLID intro & Dependency Inversion Principle

---
+++ SOLID Principles/Dependency Inversion Principle/CRUD Application/Filters/ExceptionFilters/HandleExceptionFilter.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CRUD_Application.Filters.ExceptionFilters
+{
+	public class HandleExceptionFilter : IExceptionFilter
+	{
+		private readonly ILogger<HandleExceptionFilter> _logger;
+		private readonly IHostEnvironment _hostEnvironment;
+		public HandleExceptionFilter(ILogger<HandleExceptionFilter> logger, IHostEnvironment hostEnvironment)
+		{
+			_logger = logger;
+			_hostEnvironment = hostEnvironment;
+		}
+		public void OnException(ExceptionContext context)
+		{
+			_logger.LogError
+				("ExceptionFilter {FilterName}.{MethodName}\n{ExceptionType}\n{ExceptionMessage}",
+				nameof(HandleExceptionFilter),nameof(OnException),
+				context.Exception.GetType().ToString(),nameof(context.Exception.Message));
+
+			//be careful , the specific error message should be shown only if you
+			//are in development , so end user shouldn't see these errors
+			//ShortCircuting and giving our error message details to be shown 
+			if (_hostEnvironment.IsDevelopment())
+			{
+				context.Result = new ContentResult()
+				{
+					Content = context.Exception.Message,
+					StatusCode = 500
+				};
+				//now if the exception happens in production or staging
+				//, HTTP error 500 is returned 
+			};
+
+
+		}
+	}
+}
