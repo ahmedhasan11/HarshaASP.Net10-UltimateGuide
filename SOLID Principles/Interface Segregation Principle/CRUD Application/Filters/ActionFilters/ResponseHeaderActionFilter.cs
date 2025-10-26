8728d7006e6f3cca2657d7d37a0dbdeb0d04654a
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-10-26 22:41:54 +0300

    Interface Segregation Principle for IPersonService& PersonService , also implementing them in PersonsController and add them to IOC Container

---
+++ SOLID Principles/Interface Segregation Principle/CRUD Application/Filters/ActionFilters/ResponseHeaderActionFilter.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CRUD_Application.Filters.ActionFilters
+{
+	public class ResponseHeaderActionFilter:IAsyncActionFilter
+	{
+		private readonly ILogger<ResponseHeaderActionFilter> _logger;
+		private readonly string Key;
+		private readonly string Value;
+
+		public ResponseHeaderActionFilter(ILogger<ResponseHeaderActionFilter> logger, string key, string value)
+		{
+			_logger = logger;
+			Key= key;
+			Value= value;
+		}
+
+		public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+		{
+			//structured logging
+			_logger.LogInformation("{FilterName}.{MethodName}-before", nameof(ResponseHeaderActionFilter), nameof(OnActionExecutionAsync));
+
+			await next();
+
+			//structured logging
+			_logger.LogInformation("{FilterName}.{MethodName}-after", nameof(ResponseHeaderActionFilter), nameof(OnActionExecutionAsync));
+			context.HttpContext.Response.Headers[Key] = Value;
+		}
+
+		//public void OnActionExecuted(ActionExecutedContext context)
+		//{
+		//	//structured logging
+		//	_logger.LogInformation("{FilterName}.{MethodName}",nameof(ResponseHeaderActionFilter),nameof(OnActionExecuted));
+		//}
+
+		//public void OnActionExecuting(ActionExecutingContext context)
+		//{
+		//	//structured logging
+		//	_logger.LogInformation("{FilterName}.{MethodName}", nameof(ResponseHeaderActionFilter), nameof(OnActionExecuting));
+		//	context.HttpContext.Response.Headers[Key]=Value;
+		//}
+	}
+}
---
+++ SOLID Principles/Interface Segregation Principle/Repositories/CountriesRepository.cs
@@ -0,0 +1,37 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using RepositoryContracts;
+
+namespace Repositories
+{
+	public class CountriesRepository : ICountriesRepository
+	{
+		private readonly PersonsDbContext _context;
+		public CountriesRepository(PersonsDbContext context)
+		{
+			_context= context;
+		}
+		public async Task<Country> AddCountry(Country country)
+		{
+			_context.Countries.Add(country);
+			await _context.SaveChangesAsync();
+			return country;
+		}
+
+		public async Task<List<Country>> GetAllCountries()
+		{
+			return await _context.Countries.ToListAsync();
+		}
+
+		public async Task<Country?> GetCountryByCountryID(Guid countryID)
+		{
+			return await _context.Countries.FirstOrDefaultAsync(c => c.CountryID == countryID);
+
+		}
+
+		public async Task<Country?> GetCountryByCountryName(string countryName)
+		{
+			return await _context.Countries.FirstOrDefaultAsync(c => c.Countryname == countryName);
+		}
+	}
+}
