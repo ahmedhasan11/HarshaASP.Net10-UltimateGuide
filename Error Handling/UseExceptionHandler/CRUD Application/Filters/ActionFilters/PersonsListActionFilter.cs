39a1b38837dce89fe093825c072a39fc1b060b5a
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-10-26 18:05:46 +0300

    notice difference between built-in & Custom ExceptionHandlerMiddleware , used them together to write logs and also show error view

---
+++ Error Handling/UseExceptionHandler/CRUD Application/Filters/ActionFilters/PersonsListActionFilter.cs
@@ -0,0 +1,100 @@
+﻿using CRUD_Application.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ServiceContracts.DTO;
+using ServiceContracts.Enums;
+
+namespace CRUD_Application.Filters.ActionFilters
+{
+	public class PersonsListActionFilter : IActionFilter
+	{
+		private readonly ILogger<PersonsListActionFilter> _logger;
+
+		public PersonsListActionFilter(ILogger<PersonsListActionFilter> logger)
+		{
+			_logger = logger;
+		}
+		public void OnActionExecuted(ActionExecutedContext context)
+		{
+			//AFTER logic
+			//normal logging
+			//_logger.LogInformation("PersonsListActionFilter.OnActionExecuted()"); //instead do next code
+			//Structured Logging-->
+			_logger.LogInformation("{FilterName}.{MethodName}", nameof(PersonsListActionFilter), nameof(OnActionExecuted));
+
+			PersonsController personsController = (PersonsController)context.Controller;
+
+			IDictionary<string, object?>? parameters = (IDictionary<string, object?>?)context.HttpContext.Items["arguments"];
+			if (parameters!=null)
+			{
+				if (parameters.ContainsKey("CurrentsearchBy"))
+				{
+					personsController.ViewData["CurrentsearchBy"] = Convert.ToString(parameters["CurrentsearchBy"]);
+				}
+				if (parameters.ContainsKey("CurrentsearchString") )
+				{
+					personsController.ViewData["CurrentsearchString"] = Convert.ToString(parameters["CurrentsearchString"]);
+				}
+				if (parameters.ContainsKey("CurrentsortBy") )
+				{
+					personsController.ViewData["CurrentsortBy"] = Convert.ToString(parameters["CurrentsortBy"]);
+				}
+				else
+				{
+					personsController.ViewData["CurrentsortBy"] =nameof(PersonResponse.PersonName);
+				}
+				if (parameters.ContainsKey("CurrentsortOrder"))
+				{
+					personsController.ViewData["CurrentsortOrder"] = Convert.ToString(parameters["CurrentsortOrder"]);
+				}
+				else
+				{
+					personsController.ViewData["CurrentsortOrder"] = nameof(SortOrderEnum.ASC);
+				}
+
+			}
+			personsController.ViewBag.SearchFields = new Dictionary<string, string>()
+			{
+                //right Hand Side-->which wil be displayed to the user
+                { nameof(PersonResponse.PersonName), "Person Name" },
+				{ nameof(PersonResponse.EmailAddress), "Email" },
+				{ nameof(PersonResponse.DateOfBirth), "Date Of Birth" },
+				{ nameof(PersonResponse.Gender), "Gender" },
+				{ nameof(PersonResponse.CountryID), "Country" },
+				{ nameof(PersonResponse.Address), "Address" },
+			};
+
+		}
+
+		public void OnActionExecuting(ActionExecutingContext context)
+		{
+			//provide the arguments to OnActionExecuted
+			context.HttpContext.Items["arguments"] = context.ActionArguments;
+			//BEFORE logic
+			//Structured Logging-->
+			_logger.LogInformation("{FilterName}.{MethodName}", nameof(PersonsListActionFilter), nameof(OnActionExecuting));
+
+			if (context.ActionArguments.ContainsKey("searchBy"))
+			{
+				string? searchBy = Convert.ToString(context.ActionArguments["searchBy"]);
+				if (!string.IsNullOrEmpty(searchBy))
+				{
+					var searchoptions = new List<string>()
+					{
+					nameof(PersonResponse.PersonName),
+					nameof(PersonResponse.Gender),
+					nameof(PersonResponse.Address),
+					nameof(PersonResponse.CountryID),
+					nameof(PersonResponse.DateOfBirth),
+					nameof(PersonResponse.EmailAddress),
+					};
+					if (searchoptions.Any(temp => temp == searchBy) == false)
+					{
+						_logger.LogInformation("value of {searchBy}", searchBy);
+						context.ActionArguments["searchBy"] = nameof(PersonResponse.PersonName);
+						_logger.LogInformation("value of {searchBy}", context.ActionArguments["searchBy"]);
+					}
+				}
+			}			
+		}
+	}
+}
---
+++ Error Handling/UseExceptionHandler/Services/Helpers/PersonValidationHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Helpers
+{
+   public static class PersonValidationHelper
+    {
+        internal static void ModelValidation(object obj)
+        {
+			ValidationContext validationContext = new ValidationContext(obj);
+			List<ValidationResult> validationResults = new List<ValidationResult>();
+			Validator.TryValidateObject(obj, validationContext, validationResults, true);
+		}
+	}
+}
