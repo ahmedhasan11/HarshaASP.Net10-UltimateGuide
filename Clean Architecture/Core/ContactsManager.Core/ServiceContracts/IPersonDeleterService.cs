0a4cb64b068a38a11ff90d821dc2ebde3f5a6afd
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-10-27 19:43:26 +0300

    ContactManager Project Core Section

---
+++ Clean Architecture/Core/ContactsManager.Core/ServiceContracts/IPersonDeleterService.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+using ServiceContracts.DTO;
+using ServiceContracts.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts
+{
+    public interface IPersonDeleterService
+    {
+
+       Task<bool> DeletePerson(Guid? PersonID); //bool here tell you if deletion is successful or not
+
+    }
+}
