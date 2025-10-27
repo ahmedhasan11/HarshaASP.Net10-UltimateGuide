bd5b1e876fd623691a8550c0d4b8ae588735dbc2
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-10-27 14:18:35 +0300

    Liskov Substitution Principle Notes& Understanding problems that OCP with Inheritance does

---
+++ SOLID Principles/Liskov Substitution Principle/CRUD Application/Filters/ResourceFilters/CreateDisableResourceFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CRUD_Application.Filters.ResourceFilters
+{
+	public class CreateDisableResourceFilter:IAsyncResourceFilter
+	{
+		private readonly ILogger<CreateDisableResourceFilter> _logger;
+		private readonly bool _IsDisabled;
+		public CreateDisableResourceFilter(ILogger<CreateDisableResourceFilter> logger, bool IsDisabled=true)
+		{
+		_logger = logger;
+			_IsDisabled = IsDisabled;
+		}
+
+		public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+		{
+			_logger.LogInformation("{FilterName}.{MethodName}-Before", nameof(CreateDisableResourceFilter),nameof(OnResourceExecutionAsync));
+			if (_IsDisabled)
+			{
+				context.Result = new StatusCodeResult(501); //501 is better for
+				//that you are telling the user that this button temp disabled
+			}
+			else
+			{
+				await next();
+			}
+			_logger.LogInformation("{FilterName}.{MethodName}-Before", nameof(CreateDisableResourceFilter), nameof(OnResourceExecutionAsync));
+		}
+	}
+}
---
+++ SOLID Principles/Liskov Substitution Principle/Services/PersonAdderService.cs
@@ -0,0 +1,83 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Services.Helpers;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using ServiceContracts.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using RepositoryContracts;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using SerilogTimings;
+
+
+namespace Services
+{
+	public class PersonAdderService : IPersonAdderService
+	{
+		private readonly IPersonsRepository _personsRepository;
+		private readonly ILogger<PersonGetterService> _logger;
+		private readonly IDiagnosticContext _diagnosticContext;
+		//private readonly ICountryService _countryservice;
+
+		public PersonAdderService(IPersonsRepository personsRepository, ILogger<PersonGetterService> logger,IDiagnosticContext diagnosticContext /*ICountryService countryService*/)
+		{
+			_personsRepository = personsRepository;
+			_logger = logger;
+			_diagnosticContext = diagnosticContext;
+			//_countryservice = countryService;
+		}
+		//private PersonResponse ConvertPersonToPersonResponse(Person person)
+		//{
+		//	PersonResponse personre = person.ToPersonResponse();
+		//	personre.Country = _countryservice.GetCountryByID(person.CountryID)?.Countryname;
+		//	return personre;
+		//}
+		public async Task<PersonResponse> AddPerson(PersonAddRequest? personAddRequest)
+		{
+			
+			if (personAddRequest==null)
+			{
+				throw new ArgumentNullException(nameof(personAddRequest));
+			}
+			if (string.IsNullOrEmpty(personAddRequest.PersonName))
+			{
+				throw new ArgumentException(nameof(personAddRequest.PersonName));
+			}
+			//ValidationContext validationContext = new ValidationContext(personAddRequest);
+			//List<ValidationResult> validationResults = new List<ValidationResult>();
+			//Validator.TryValidateObject(personAddRequest, validationContext, validationResults, true);
+
+			//instead of the code above
+			PersonValidationHelper.ModelValidation(personAddRequest);
+			Person person = personAddRequest.ToPerson();
+
+			person.PersonID = Guid.NewGuid();
+
+			//_db.sp_InsertPerson(person);
+
+			 await _personsRepository.AddPerson(person);
+
+			//person
+			PersonResponse response = person.ToPersonResponse();
+			//herewe want to get the country name from countryID ,so we candothis by usingthe GetCountryByID
+			//function we created in CountryService
+			//but you havetoinject it in your constructor to use it
+			
+			//response.Country = _countryservice.GetCountryByID(person.CountryID)?.Countryname;
+			//if you didnt put the "?" here it will give yopu null ref error , what the "?" do here
+			//it says if that _countryservice.GetCountryByID(person.CountryID) returned null , dont access
+			//this countryname property , and assign null to the Country peroperty 
+
+			return response;
+		}
+	}
+}
